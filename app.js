a4aee0ee790da927398de7cea71598b56712c92e
abrilcapizzano <103966017+abrilcapizzano@users.noreply.github.com>
2023-06-19 20:42:34 -0300

    modificasciones finales

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ app.listen(port, ()=>{
     console.log(`Example app listening on port ${port}`)
 })
 app.use(express.urlencoded({ extended: false }));
+
 const productosRoutes = require('./routes/productosRoutes.js');
 
 app.use('/', productosRoutes);
-
 app.set('view engine', 'hbs');
 app.use("/assets", express.static(__dirname + "/public"));
